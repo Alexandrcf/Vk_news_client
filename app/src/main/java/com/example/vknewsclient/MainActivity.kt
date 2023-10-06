59705ea355046f6968bd2091472898ce6587fb8a
a.lukyanov <a.lukyanov@ritm.media>
2023-10-07 01:00:39 +0300

    Добавил экран PostCard

--- app/src/main/java/com/example/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/example/vknewsclient/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.vknewsclient.ui.theme.PostCard
 import com.example.vknewsclient.ui.theme.VkNewsClientTheme
 
@@ -24,6 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier
                         .fillMaxSize()
                         .background(MaterialTheme.colorScheme.background)
+                        .padding(8.dp)
                 ) {
                     PostCard()
                 }
--- app/src/main/java/com/example/vknewsclient/ui/theme/PostCard.kt
+++ app/src/main/java/com/example/vknewsclient/ui/theme/PostCard.kt
@@ -1,6 +1,7 @@
 package com.example.vknewsclient.ui.theme
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,52 +15,141 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.AccountCircle
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.vknewsclient.R
 
-@Preview
 @Composable
 fun PostCard() {
     Card {
-        Row(
-            Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically
+        Column(
+            Modifier.padding(8.dp)
         ) {
+            PostHeader()
+            Spacer(
+                modifier = Modifier
+                    .height(8.dp)
+            )
+            Text(text = "Some text sofdjugrujkdfnhwguqgdbjnm,cmniwuedfijiownfm")
             Image(
                 modifier = Modifier
-                    .size(50.dp)
-                    .clip(CircleShape),
-                imageVector = Icons.Rounded.AccountCircle,
-                contentDescription = null
+                    .fillMaxWidth(),
+                painter = painterResource(R.drawable.post_comunity_thumbnail),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth
             )
             Spacer(
                 modifier = Modifier
-                    .width(8.dp)
+                .height(8.dp)
             )
-            Column(
-                modifier = Modifier
-                    .weight(1f)
-            ) {
-                Text(text = "Dev/null")
-                Spacer(
-                    modifier = Modifier
-                        .height(4.dp)
-                )
-                Text(text = "14:00")
-
-            }
-            Image(
-                imageVector = Icons.Rounded.MoreVert,
-                contentDescription = null
-            )
+            Statistics()
         }
     }
+}
 
+@Composable
+fun PostHeader() {
+    Row(
+        Modifier
+            .fillMaxWidth(),
+        verticalAlignment = Alignment . CenterVertically
+    ) {
+        Image(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape),
+            imageVector = Icons.Rounded.AccountCircle,
+            contentDescription = null
+        )
+        Spacer(
+            modifier = Modifier
+                .width(8.dp)
+        )
+        Column(
+            modifier = Modifier
+                .weight(1f)
+        ) {
+            Text(
+                text = "Dev/null",
+                color = MaterialTheme.colorScheme.onPrimary
+            )
+            Spacer(
+                modifier = Modifier
+                    .height(4.dp)
+            )
+            Text(
+                text = "14:00",
+                color = MaterialTheme.colorScheme.onSecondary
+            )
+
+        }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+    }
+}
+
+@Composable
+fun Statistics() {
+    Row() {
+        Row(
+            modifier =  Modifier.weight(1f)
+        ) {
+            IconWithText(iconResId = R.drawable.ic_views_count, text = "966")
+        }
+
+        Row(
+            modifier =  Modifier.weight(1f),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconWithText(iconResId = R.drawable.ic_share, text = "9")
+            IconWithText(iconResId = R.drawable.ic_comment, text = "8")
+            IconWithText(iconResId = R.drawable.ic_like, text = "9")
+        }
+    }
+}
+
+@Composable
+fun IconWithText(iconResId: Int, text: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = iconResId),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.onSecondary
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = text,
+            color = MaterialTheme.colorScheme.onSecondary
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewLight() {
+    VkNewsClientTheme(false) {
+        PostCard()
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewDark() {
+    VkNewsClientTheme(true) {
+        PostCard()
+    }
 }

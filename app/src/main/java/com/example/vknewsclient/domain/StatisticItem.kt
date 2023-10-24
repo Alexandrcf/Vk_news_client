6b88d054fa97e0b95985097c6095254218a714e7
a.lukyanov <a.lukyanov@ritm.media>
2023-10-24 23:45:28 +0300

    Добавил domain слой

---
+++ app/src/main/java/com/example/vknewsclient/domain/StatisticItem.kt
@@ -0,0 +1,14 @@
+package com.example.vknewsclient.domain
+
+data class StatisticItem(
+    val type: StatisticsType,
+    val count: Int = 0,
+) {
+}
+
+enum class StatisticsType {
+    VIEWS,
+    COMMENTS,
+    SHARES,
+    LIKES,
+}
